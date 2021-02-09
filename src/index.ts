3a53f2559e73b2f2a646cf9571796afec3477195
otaviobonder <otaviobonder@gmail.com>
2021-02-09 16:24:12 -0300

    added database instance and fix graphql syntax

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { ApolloServer, gql } from 'apollo-server';
 import { GraphQLDateTime } from 'graphql-iso-date';
+import prisma from './database';
+
+const serverPort = 4001;
 
 const typeDefs = gql`
   scalar Date
 
-  # This entity represent all Grayscale purchases
+  # This entity represents all Grayscale purchases
   type Grayscale {
     id: ID!
     date: Date!
@@ -12,12 +15,19 @@ const typeDefs = gql`
   }
 
   type Query {
-    purchases: [Grayscale!]
+    allPurchases: [Grayscale!]!
   }
 `;
 
 const resolvers = {
   Date: GraphQLDateTime,
+  Query: {
+    allPurchases: () => prisma.purchases.findMany(),
+  },
 };
 
-const server = new ApolloServer({});
+const server = new ApolloServer({ resolvers, typeDefs });
+
+server.listen({ port: serverPort }, () => {
+  console.log(`Server listening at ${serverPort}`); // eslint-disable-line
+});
